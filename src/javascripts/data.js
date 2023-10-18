e80c7f283629735d18a3a64ae5ac997ccf91daff
Danny Laurence <danny.laurence@expeditiondata.com>
2023-10-17 20:52:04 -0400

    Dynamic javascript with professional work and semester information

---
+++ src/javascripts/data.js
@@ -0,0 +1,27 @@
+export const semesterData = [{
+  year: '2019 Fall Semester',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+}, {
+  year: '2020 Fall Semester',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+}, {
+  year: '2021 Fall Semester',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+}, {
+  year: '2023 Fall Semester',
+  description: 'Obviously the best semester!',
+}];
+
+export const companyData = [{
+  name: 'Dante Consulting',
+  externalLink: 'http://www.danteinc.com/',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+}, {
+  name: 'Booz Allen Hamilton',
+  externalLink: 'https://www.boozallen.com/',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+}, {
+  name: 'Team Engine',
+  externalLink: 'https://www.teamengine.io',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+}];
--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,1 +1,30 @@
-console.log('Hello from index.js!');
+import { semesterData, companyData } from './data';
+
+const semesterCardHolder = document.getElementById('semester-card-holder');
+for (let i = 0; i < 3; i += 1) {
+  const semester = semesterData[i];
+
+  const divContainer = document.createElement('div');
+  divContainer.setAttribute('class', 'card');
+  divContainer.innerHTML = `
+    <h3>${semester.year}</h3>
+    <p>${semester.description}</p>
+  `;
+
+  semesterCardHolder.appendChild(divContainer);
+}
+
+const professionalCardHolder = document.getElementById('professional-card-holder');
+for (let i = 0; i < 3; i += 1) {
+  const company = companyData[i];
+
+  const divContainer = document.createElement('div');
+  divContainer.setAttribute('class', 'card');
+  divContainer.innerHTML = `
+    <h3>${company.name}</h3>
+    <a href="${company.externalLink}" target="_blank">Learn More</a>
+    <p>${company.description}</p>
+  `;
+
+  professionalCardHolder.appendChild(divContainer);
+}
